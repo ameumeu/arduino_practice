3062a529d3abeab0e45dda28cdb500131615dab3
ameumeu <83584754+ameumeu@users.noreply.github.com>
2021-11-10 09:19:26 +0900

    Add files via upload

---
+++ segment/segment.ino
@@ -0,0 +1,44 @@
+byte digits[10][7] =
+{
+  { 0, 0, 0, 0, 0, 0, 1 }, // 0 // 에노드 기준
+  { 1, 0, 0, 1, 1, 1, 1 }, // 1 // 캐소드로 바꾸려면 반대로 출력해야함
+  { 0, 0, 1, 0, 0, 1, 0 }, // 2
+  { 0, 0, 0, 0, 1, 1, 0 }, // 3
+  { 1, 0, 0, 1, 1, 0, 0 }, // 4
+  { 0, 1, 0, 0, 1, 0, 0 }, // 5
+  { 0, 1, 0, 0, 0, 0, 0 }, // 6
+  { 0, 0, 0, 1, 1, 1, 1 }, // 7
+  { 0, 0, 0, 0, 0, 0, 0 }, // 8
+  { 0, 0, 0, 1, 1, 0, 0 }  // 9
+};
+int arr[27]={3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3,8,4,6,2,6,4,3,3,8};
+
+void setup() {
+  for (int i = 2; i < 10; i++) {
+    pinMode(i, OUTPUT);
+  }
+  digitalWrite(9, !HIGH); // ! 붙여서 캐소드 방식에서 DP 끄기
+}
+
+void loop() {
+//  for (int i = 0; i < 10; i++) {    
+//    displayDigit(i);
+//    delay(1000);
+//  }
+  for (int i=0; i<27; i++){
+    displayDigit(arr[i]);
+    if (i==0) {
+      digitalWrite(9, HIGH);
+    } else {
+      digitalWrite(9, LOW);
+    }
+    delay(500);
+    }
+ }
+
+void displayDigit(int num) {
+  int pin = 2;
+  for (int i = 0; i < 7; i++) {
+    digitalWrite(pin + i, !digits[num][i]); // !를 붙여서 에노드 -> 캐소드
+  }
+}
---
+++ segment_button/segment_button.ino
@@ -0,0 +1,56 @@
+int digit = 0;
+byte digits[10][7] =
+{
+  { 0, 0, 0, 0, 0, 0, 1 }, // 0
+  { 1, 0, 0, 1, 1, 1, 1 }, // 1
+  { 0, 0, 1, 0, 0, 1, 0 }, // 2
+  { 0, 0, 0, 0, 1, 1, 0 }, // 3
+  { 1, 0, 0, 1, 1, 0, 0 }, // 4
+  { 0, 1, 0, 0, 1, 0, 0 }, // 5
+  { 0, 1, 0, 0, 0, 0, 0 }, // 6
+  { 0, 0, 0, 1, 1, 1, 1 }, // 7
+  { 0, 0, 0, 0, 0, 0, 0 }, // 8
+  { 0, 0, 0, 1, 1, 0, 0 },  // 9
+
+};
+int foo[3][7]={
+    {0,1,1,1,1,1,0},//F
+    { 0, 0, 0, 0, 0, 0, 1 },
+    { 0, 0, 0, 0, 0, 0, 1 }
+};
+
+void setup() {
+  pinMode(11, INPUT);
+  pinMode(12, INPUT);
+
+  for (int i = 2; i < 10; i++) {
+    pinMode(i, OUTPUT);
+  }
+  digitalWrite(9, !HIGH);
+}
+
+void loop() {
+  if (digitalRead(11) == HIGH) {
+    ++digit;
+    if (digit > 3) {
+      digit = 0;
+    }
+  }
+
+  if (digitalRead(12) == HIGH) {
+    --digit;
+    if (digit < 0) {
+      digit = 3;
+    }
+  }
+
+  displayDigit(digit);
+  delay(200);
+}
+
+void displayDigit(int num) {
+  int pin = 2;
+  for (int i = 0; i < 7; i++) {
+    digitalWrite(pin + i, !foo[num][i]);//digits[num][i]
+  }
+}
